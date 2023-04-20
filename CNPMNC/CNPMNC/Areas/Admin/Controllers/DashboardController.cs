90888accd18e268022d3ed7993f38824aed1615e
Tafi <havinhtai712@gmail.com>
2023-04-20 22:40:42 +0700

    Thêm trang doanh thu thống kê

---
+++ CNPMNC/CNPMNC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using CNPMNC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPMNC.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        CNPMNCEntities db = new CNPMNCEntities();
+        // GET: Admin/Dashboard
+        public ActionResult Dashboard()
+        {
+            var model = new Dashboard();
+            model.TotalUsers = db.KHACHHANGs.Count();
+            model.TotalAdmin = db.ADMINs.Count();
+            model.TotalNewOrders = db.DONHANGs.Where(x => x.TRANGTHAIID == 1).Count();
+            model.TotalOrders = db.DONHANGs.Where(x => x.TRANGTHAIID == 2).Count();
+            model.TotalProducts = db.DIENTHOAIs.Count();
+            model.TotalRevenue = db.DONHANGs.Where(x => x.TRANGTHAIID == 2)
+                                     .Sum(x => (decimal?)x.THANHTIEN) ?? 0;
+
+            return View(model);
+        }
+    }
+}
---
+++ CNPMNC/CNPMNC/Models/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CNPMNC.Models
+{
+    public class Dashboard
+    {
+        public int TotalUsers { get; set; }
+        public int TotalAdmin{ get; set; }
+        public int TotalNewOrders { get; set; }
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0,0 đ}", ApplyFormatInEditMode = false)]
+        public Nullable<decimal> TotalRevenue { get; set; }
+    }
+}
