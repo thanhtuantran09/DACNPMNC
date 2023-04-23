03ab4131c5bff14725707dbe602c804ad47b0c53
Tafi <havinhtai712@gmail.com>
2023-04-24 00:00:51 +0700

    thêm biểu đồ ở dashboard

--- CNPMNC/CNPMNC/Areas/Admin/Controllers/DashboardController.cs
+++ CNPMNC/CNPMNC/Areas/Admin/Controllers/DashboardController.cs
@@ -1,9 +1,15 @@
 ﻿using CNPMNC.Models;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.UI.DataVisualization.Charting;
+using Chart = System.Web.UI.DataVisualization.Charting.Chart;
 
 namespace CNPMNC.Areas.Admin.Controllers
 {
@@ -23,6 +29,34 @@ namespace CNPMNC.Areas.Admin.Controllers
                                      .Sum(x => (decimal?)x.THANHTIEN) ?? 0;
 
             return View(model);
+        }
+        
+        public ActionResult Chart()
+        {
+            var data = db.DONHANGs.ToList();
+            var chart = new Chart();
+            var area = new ChartArea();
+            chart.ChartAreas.Add(area);
+            var series = new Series();
+            var year = DateTime.Now.Year;
+            var ordersByMonth = data.GroupBy(x => x.NGAYTAO.Value.Month);
+            foreach (var item in ordersByMonth)
+            {
+                var month = item.Key;
+                var totalRevenue = item.Sum(x => x.THANHTIEN);
+                series.Points.AddXY(month, totalRevenue);
+            }
+            chart.Titles.Add($"Doanh thu theo tháng năm {year}");
+            series.Label = "#PERCENT{P0}";
+            series.Font = new Font("Arial", 8.0f, FontStyle.Bold);
+            series.ChartType = SeriesChartType.Column;
+            series["PieLabelStyle"] = "Outside";
+            chart.Series.Add(series);
+            var returnStream = new MemoryStream();
+            chart.ImageType = ChartImageType.Png;
+            chart.SaveImage(returnStream);
+            returnStream.Position = 0;
+            return new FileStreamResult(returnStream, "image/png");
         }
     }
 }
